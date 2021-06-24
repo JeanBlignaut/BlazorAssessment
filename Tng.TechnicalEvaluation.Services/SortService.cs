e2ca9fbe7cb04f5b3a705a25d98a8efc846913f2
Jean Blignaut <jean.blignaut@me.com>
2021-06-24 19:07:42 +0200

    Sort Implementation - broken

--- Tng.TechnicalEvaluation.Services/SortService.cs
+++ Tng.TechnicalEvaluation.Services/SortService.cs
@@ -1,5 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Tng.TechnicalEvaluation.Infrastructure.Services;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Tng.TechnicalEvaluation.Services
 {
@@ -7,14 +10,48 @@ namespace Tng.TechnicalEvaluation.Services
     {
         public string BubbleSort(string input)
         {
-            //TODO - Complete this
-            return input;
+            var items = input.Split(',');
+
+            for (int o = 0; o < items.Length - 1; o++)
+            {
+                for (int i = 0; i < items.Length - i - 1; i++)
+                {
+                    if (string.Compare(items[i], items[i + 1], System.StringComparison.InvariantCultureIgnoreCase) > 0)
+                    {
+                        var temp = items[i];
+                        items[i] = items[i + 1];
+                        items[i + 1] = temp;
+                    }
+
+                    Debug.Print($"o={o}, i={i}");
+
+                }
+            }
+
+            return string.Join(',', items);
         }
 
         public string SelectSort(string input)
         {
-            //TODO - Complete this
-            return input;
+            string[] items = input.Split(',');
+            var result = new List<string>();
+            int itemsOriginalLength = items.Length;
+            int selectedIndex = 0;
+
+            while (result.Count() < itemsOriginalLength)
+            {
+                for (int i = 1; i < items.Length; i++)
+                {
+                    if (string.Compare(items[selectedIndex], items[i], System.StringComparison.InvariantCultureIgnoreCase) > 0)
+                    {
+                        selectedIndex = i;
+                    }
+                }
+                result.Append(items[selectedIndex]);
+                items = items.Except(result).ToArray();
+            }
+
+            return string.Join(',', items);
         }
     }
 }
